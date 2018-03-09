6433091f9eb0c937b59321e4b63d09b7c4317289
Kenta Takatsu <kt426@cornell.edu>
2018-03-08 19:51:16 -0500

    text encoding

--- layers.py
+++ layers.py
@@ -1,9 +1,28 @@
-'''
+"""
 Layers
-'''
+"""
+import tensorflow as tf
 
-def embedding(input):
-    pass
+
+def encoding(word, char, word_embeddings, char_embeddings, scope = "embedding"):
+    """Emcode the list of word ids and character ids to pretrained embeddings using
+    tf.nn.embedding_lookup
+    Args:
+        word: list of word ids
+        char: list of char ids
+        word_embeddings: pretrained nxm matrix where n = number of vocabulary
+        and m = embedding dimension (300)
+        char_embeddings: pretrained nxm matrix where n = number of vocabulary
+        and m = embedding dimension (300)
+        scope:
+
+    Returns:
+        word_encoding, char_encoding
+    """
+    with tf.variable_scope(scope):
+        word_encoding = tf.nn.embedding_lookup(word_embeddings, word)
+        char_encoding = tf.nn.embedding_lookup(char_embeddings, char)
+        return word_encoding, char_encoding
 
 
 def bidirectional_rnn(input):
