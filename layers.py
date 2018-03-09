8ffa957fce579b364afb58862a731c3cbc979dd7
yujiakimoto <ya242@cornell.edu>
2018-03-08 20:00:27 -0500

    Update docstring

--- layers.py
+++ layers.py
@@ -1,22 +1,25 @@
 """
 Layers
 """
+
 import tensorflow as tf
+import numpy as np
 
 
-def encoding(word, char, word_embeddings, char_embeddings, scope = "embedding"):
-    """Encode the list of word ids and character ids to pretrained embeddings using
-    tf.nn.embedding_lookup
+def encoding(word, char, word_embeddings, char_embeddings, scope="embedding"):
+    """Encode the list of word ids and character ids to pretrained embeddings using tf.nn.embedding_lookup
     Args:
-        word: list of word ids
-        char: list of char ids
-        word_embeddings: pretrained nxm matrix where n = number of vocabulary
-        and m = embedding dimension (by default 300)
-        char_embeddings: pretrained nxm matrix where n = number of vocabulary
-        and m = embedding dimension (by default 300)
+        word (list):                  list of word ids
+        char (list):                  list of char ids
+        word_embeddings (np.ndarray): pretrained nxm matrix where n = number of vocabulary and
+                                      m = embedding dimension (default 300)
+        char_embeddings (np.ndarray): pretrained nxm matrix where n = number of vocabulary and
+                                      m = embedding dimension (default 300)
+        scope (str):                  tensorflow variable scope
     
     Returns:
-        word_encoding, char_encoding
+        word_encoding (tensor)
+        char_encoding (tensor)
     """
     with tf.variable_scope(scope):
         word_encoding = tf.nn.embedding_lookup(word_embeddings, word)
