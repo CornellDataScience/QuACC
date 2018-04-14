518f923843454045fafafe22522fa5b2e0234327
yujiakimoto <ya242@cornell.edu>
2018-04-14 16:53:44 -0400

    Update training script

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ def train():
     # build tensorflow graph
     quacc = Model()
     saver = tf.train.Saver()
-    best_val_acc = 0
+    best = 0
 
     # initialize graph
     init = tf.global_variables_initializer()
@@ -25,13 +25,13 @@ def train():
     with tf.Session() as sess:
         merged = tf.summary.merge_all()
         train_writer = tf.summary.FileWriter(os.path.join(Hp.log_dir, 'train'), sess.graph)
-        test_writer = tf.summary.FileWriter(os.path.join(Hp.log_dir, 'val'))
+        # test_writer = tf.summary.FileWriter(os.path.join(Hp.log_dir, 'val'))
         sess.run(init)
 
         for ep in tqdm(range(Hp.n_epochs)):
             tr_loss, tr_acc = 0, 0
 
-            for itr in tqdm(range(data.batches_tr['n_batches'])):
+            for i in tqdm(range(data.batches_tr['n_batches'])):
                 # get next batch
                 p_batch, q_batch, p_lengths, q_length, ptr_batch = data.next_training_batch()
                 # build a feed dict
@@ -44,10 +44,35 @@ def train():
                 tr_loss += loss
                 tr_acc += acc
 
-                if itr % 100 == 0:
+                if i % 100 == 0:
                     # tensorboard
                     summary = sess.run(merged, feed_dict=train_dict)
-                    train_writer.add_summary(summary, itr)
+                    train_writer.add_summary(summary, i)
+
+            tr_loss /= data.batches_tr['n_batches']
+            tr_acc /= data.batches_tr['n_batches']
+
+            if ep % 10 == 0:
+                print('Validating...')
+                va_acc = 0
+                for _ in tqdm(range(data.batches_va['n_batches'])):
+                    # get next batch
+                    p_batch, q_batch, p_lengths, q_length, ptr_batch = data.next_validation_batch()
+                    # build a feed dict
+                    val_dict = {quacc.p_word_inputs: p_batch,
+                                quacc.q_word_inputs: q_batch,
+                                quacc.p_word_lengths: p_lengths,
+                                quacc.q_word_lengths: q_length,
+                                quacc.labels: ptr_batch}
+                    acc, = sess.run(quacc.exact_match, feed_dict=val_dict)
+                    va_acc += acc
+
+                va_acc /= data.batches_va['n_batches']
+                if va_acc > best:
+                    best = va_acc
+                    saver.save(sess, './models')
+                else:
+                    saver.restore(sess, './models')
 
 
 if __name__ == '__main__':
